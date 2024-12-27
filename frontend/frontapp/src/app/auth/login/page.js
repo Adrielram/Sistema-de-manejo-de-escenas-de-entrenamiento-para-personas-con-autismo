86cbbc5b6434e545020df599b2c243eb5c040df5
NicolasBonelli <nico.bonellidelhoyo@gmail.com>
2024-12-27 16:48:09 -0300

    Añadido de interfaz de login

---
+++ frontend/frontapp/src/app/auth/login/page.js
@@ -0,0 +1,28 @@
+// pages/auth/login.js
+import LoginForm from '../../../components/forms/LoginForm';
+import Image from 'next/image';
+export default function LoginPage() {
+  return (
+    <div
+      className="flex justify-center items-center min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/images/imagenFondoLogin.jpg')",
+      }}
+    >
+      <div className="w-full max-w-md p-8 bg-white border-2 border-black border-solid rounded-lg shadow-lg">
+        <div className="flex justify-center mb-6">
+        <div className="border-2 border-black rounded-full p-1">
+            <Image
+              src="/images/logoCasabella.png" 
+              alt="Logo Casa Bella"
+              width={120} 
+              height={120} 
+              className="rounded-full"
+            />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
--- frontend/frontapp/src/app/page.tsx
+++ frontend/frontapp/src/app/page.tsx
@@ -12,15 +12,15 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
+        <ol className="p-4 w-64 border-none border-b border-b-[#a7bcff] placeholder:text-gray-400 focus:outline-none">
+          <li className="p-4 w-64 border-none border-b border-b-[#94003b] placeholder:text-gray-400 focus:outline-none">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <code className="bg-black/[.05] dark:bg-red/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="text-red-500">Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
