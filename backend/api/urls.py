c615b89dc91e41b0d727f413358003a2749a5f7a
NicolasBonelli <nico.bonellidelhoyo@gmail.com>
2024-12-31 10:46:49 -0300

    Agregado de principio de cosas para sesion

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login, logout
 
 urlpatterns = [
     # Definir las rutas para las vistas que tengas en tu aplicación
     path('example/', views.example_view, name='example'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 def example_view(request):
     return JsonResponse({'message': 'Hello, world!'})
+
+@api_view(['POST'])
+def login(request):
+    serializer = TokenObtainPairSerializer(data=request.data)
+    if serializer.is_valid():
+        response = Response({"message": "Login successful"})
+        # Almacenar el token de acceso en una cookie HTTP-only
+        response.set_cookie(
+            'jwt',  # Nombre de la cookie
+            serializer.validated_data['access'],  # Token JWT
+            httponly=True,  # Previene acceso desde JavaScript
+            samesite='Lax'  # Mejora seguridad contra ataques CSRF
+        )
+        return response
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['POST'])
+def logout(request):
+    response = Response({"message": "Logout successful"})
+    # Borrar la cookie JWT
+    response.delete_cookie('jwt')
+    return response
