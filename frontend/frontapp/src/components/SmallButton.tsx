6e8a5d54971f37409754ee25c1d6e25ef9fa46b1
Mateo <matetornikato@gmail.com>
2025-01-03 08:29:27 -0300

    Fix del scroll y uso de small_button modificado

--- frontend/frontapp/src/components/SmallButton.tsx
+++ frontend/frontapp/src/components/SmallButton.tsx
@@ -8,11 +8,12 @@ type ButtonProps = {
     title: string;
     hover?: string;
     onClick?: () => void;
+    className?: string;
 }
 
-const Button = ({ color, font_bold, title, hover, onClick }: ButtonProps) => {
+const Button = ({ color, font_bold, title, hover, onClick ,className}: ButtonProps) => {
   return (    
-    <button className={`${color || "bg-primary"} bg border-none rounded-xl cursor-pointer text-base px-3 py-2 ${font_bold || "font-normal"} font-sans text-white ${hover || "hover:bg-primary-dark"} `} onClick={onClick}>
+    <button className={`${color || "bg-primary"} bg border-none rounded-xl cursor-pointer text-base px-3 py-2 ${font_bold || "font-normal"} font-sans text-white ${hover || "hover:bg-primary-dark"}  ${className} `} onClick={onClick}>
       {title}
     </button>
   )
