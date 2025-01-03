e330ecb52ed442791a08115a4bcce028dc9805c3
TomasRodriguez2002 <tomasrodriguez02002@gmail.com>
2025-01-03 09:21:17 -0300

    create component BigButton

--- frontend/frontapp/src/components/SmallButton.tsx
+++ frontend/frontapp/src/components/SmallButton.tsx
@@ -10,12 +10,19 @@ type ButtonProps = {
     onClick?: () => void;
 }
 
-const Button = ({ color, font_bold, title, hover, onClick }: ButtonProps) => {
+const SmallButton = ({ color, font_bold, title, hover, onClick }: ButtonProps) => {
   return (    
-    <button className={`${color || "bg-primary"} bg border-none rounded-xl cursor-pointer text-base px-3 py-2 ${font_bold || "font-normal"} font-sans text-white ${hover || "hover:bg-primary-dark"} `} onClick={onClick}>
+    <button 
+      className={`
+        ${color || "bg-primary"} 
+        bg border-none rounded-xl cursor-pointer text-base px-3 py-2 
+        ${font_bold || "font-normal"} font-sans text-white 
+        ${hover || "hover:bg-primary-dark"} 
+        `} 
+      onClick={onClick}>
       {title}
     </button>
   )
 }
 
-export default Button
+export default SmallButton
