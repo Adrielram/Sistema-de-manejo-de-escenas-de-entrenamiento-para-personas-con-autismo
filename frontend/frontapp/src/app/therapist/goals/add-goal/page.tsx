1aae7434c35e1f7caadef090155a0b8b9cc6401a
HectorMino <paquisaya@gmail.com>
2025-01-08 11:09:51 -0300

    fixing add_obj box

--- frontend/frontapp/src/app/therapist/goals/add-goal/page.tsx
+++ frontend/frontapp/src/app/therapist/goals/add-goal/page.tsx
@@ -45,9 +45,9 @@ const CreateObjetivo: React.FC = () => {
     setEscenaId("");
   };
   return (
-    <div className="max-w-xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md mt-10 ">
-      <h1 className="text-3xl font-bold text-blue-700 mb-6 text-center">
-        Crear Objetivo
+    <div className="max-w-xl mx-auto p-6 bg-gray-100 border-4 border-[#3EA5FF] rounded-lg shadow-2xl mt-20 ">
+      <h1 className="text-3xl font-bold text-[#3EA5FF] mb-6 text-center  px-4 py-2 rounded">
+      Crear Objetivo
       </h1>
       <form onSubmit={handleSubmit} className="space-y-6">
         <div>
@@ -62,7 +62,7 @@ const CreateObjetivo: React.FC = () => {
             type="text"
             value={titulo}
             onChange={(e) => setTitulo(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-[#3EA5FF]"
             placeholder="Ingrese el título"
           />
         </div>
@@ -77,7 +77,7 @@ const CreateObjetivo: React.FC = () => {
             id="descripcion"
             value={descripcion}
             onChange={(e) => setDescripcion(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-[#3EA5FF]"
             placeholder="Ingrese la descripción"
           />
         </div>
@@ -93,13 +93,13 @@ const CreateObjetivo: React.FC = () => {
             type="text"
             value={escenaId}
             onChange={(e) => setEscenaId(e.target.value)}
-            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full border border-gray-300 rounded-lg p-3 text-gray-800 focus:outline-none focus:ring-2 focus:ring-[#3EA5FF]"
             placeholder="Ingrese el ID de la escena"
           />
         </div>
         <button
           type="submit"
-          className="w-full bg-blue-600 text-white font-semibold py-3 rounded-lg hover:bg-blue-700 transition duration-300"
+          className="w-full bg-[#3EA5FF] text-white font-semibold py-3 rounded-lg hover:bg-[#2E8BFF] transition duration-300"
         >
           Crear Objetivo
         </button>
