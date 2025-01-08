737a8e93e9b74edaebf5859e157fd684c6afb879
TomasRodriguez2002 <tomasrodriguez02002@gmail.com>
2025-01-07 21:08:16 -0300

    sidenav, header and footer fixed

--- frontend/frontapp/src/app/layout.tsx
+++ frontend/frontapp/src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
 // import { Geist, Geist_Mono } from "next/font/google";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 import "./globals.css";
-import SideNav from "../components/SideNav";
-import MarginWidthWrapper from "../components/MarginWidthWrapper";
-import HeaderMobile from "../components/MobileHeader";
-import PageWrapper from "../components/PageWrapper";
-
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,45 +12,12 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}){
   return (
     <html lang="en">
-      <body className={`min-h-screen bg-white ${inter.className}`}>
-        <Header /> {/* Asegúrate de que esté fuera del contenedor flex */}
-        <div className="flex pt-[47px]">
-          <SideNav />
-          <main className="flex-1 mt-14">
-            <MarginWidthWrapper>
-              <HeaderMobile />
-              <PageWrapper>{children}</PageWrapper>
-            </MarginWidthWrapper>
-          </main>
-        </div>
-        <Footer />
+      <body>
+        <main className = "mt-14">{children}</main>
       </body>
     </html>
   );
 }
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }){
-//   return (
-//     <html lang="en">
-//       <body className={`bg-white ${inter.className}`}>
-//         <div className="flex">
-//           <SideNav />
-//           <main className="flex-1">
-//             <MarginWidthWrapper>
-//               <MenuHeader /> {/*aca va el header real*/}
-//               <HeaderMobile />
-//               <PageWrapper>{children}</PageWrapper>
-//             </MarginWidthWrapper>
-//           </main>
-//         </div>
-//       </body>
-//     </html>
-//   );
-// }
