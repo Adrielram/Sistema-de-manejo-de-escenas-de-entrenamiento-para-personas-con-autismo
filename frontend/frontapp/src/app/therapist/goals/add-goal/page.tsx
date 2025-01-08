775f7077ab2af52d20ae4c22585f0ea2706763e0
HectorMino <paquisaya@gmail.com>
2025-01-08 10:19:24 -0300

    AAA

--- frontend/frontapp/src/app/therapist/goals/add-goal/page.tsx
+++ frontend/frontapp/src/app/therapist/goals/add-goal/page.tsx
@@ -45,7 +45,7 @@ const CreateObjetivo: React.FC = () => {
     setEscenaId("");
   };
   return (
-    <div className="max-w-xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+    <div className="max-w-xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md mt-10 ">
       <h1 className="text-3xl font-bold text-blue-700 mb-6 text-center">
         Crear Objetivo
       </h1>
