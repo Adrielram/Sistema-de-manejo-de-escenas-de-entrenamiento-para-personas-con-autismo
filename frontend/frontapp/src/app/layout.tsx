76667369f18b9b06c1eec2e623b6098d8e3a2d0a
Adrielram <adriel_ram@hotmail.com>
2025-01-09 10:53:20 -0300

    layout global agregado

---
+++ frontend/frontapp/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+
+import { Inter } from 'next/font/google';
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import PageWrapper from "../components/PageWrapper";
+
+// Asegúrate de importar ReduxProvider desde la ruta correcta
+import ReduxProvider from "../../store/provider";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: "Centro Casabella",
+  description: "Entorno de entrenamiento para personas con autismo",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}){
+  return (
+    <html lang="en">
+      <body className={`min-h-screen bg-white ${inter.className}`}>
+        <ReduxProvider>
+        <div className="flex pt-[47px]">
+          <main className="flex-1 mt-14">
+              <PageWrapper>{children}</PageWrapper>
+          </main>
+        </div>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
