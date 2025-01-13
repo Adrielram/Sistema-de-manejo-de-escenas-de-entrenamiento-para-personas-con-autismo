6135f621405e42492f58eda4bc277e7010ff5cfa
Mateo <matetornikato@gmail.com>
2025-01-13 13:25:53 -0300

    crear nueva rama verSeguimientoHijo

---
+++ frontend/frontapp/src/app/interfaz_padre/ver_seguimiento/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+const verSeguimiento = () => {
+  return (
+    <div className='text-black'>verSeguimiento</div>
+  )
+}
+
+export default verSeguimiento
