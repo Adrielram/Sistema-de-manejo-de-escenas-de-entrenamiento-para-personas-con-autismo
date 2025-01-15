46869efdac98c990b6890311f9a621cfcac95399
HectorMino <paquisaya@gmail.com>
2025-01-15 15:07:58 -0300

    last commit to keep therapist updated with the model.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,9 +20,7 @@ urlpatterns = [
     path('pacientes/', PacienteListView.as_view(), name='paciente-list'),
     path('objetivo/<int:objetivo_id>/', get_goal_data, name='get_goal_data'),
     path('signIn/', views.signIn, name='signIn'),
-<<<<<<<<< Temporary merge branch 1
     path('buscar_padres/', views.buscar_padres, name='buscar_padres'),
-=========
     path('crear-escena/', views.crear_escena, name='crear_escena'),
     path('escenas/', EscenaListView.as_view(), name='escena-list'),
     path('objetivos-list/', ObjetivosListView.as_view(), name='objetivo-list'),
