dbc56cc1b1a05d730750833324e39cfd933a4b9b
tommG2002 <133533859+tommG2002@users.noreply.github.com>
2025-01-21 13:20:33 -0300

    Update serializers.py
    Evitar Duplicados de imports

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from .models import User, Objetivo, Escena, CentroProfesional, Centrodesalud, Grupo
 from .models import User, Objetivo, Escena, CentroProfesional, Centrodesalud, Comentario, Videosvistos, Grupo
 
 # Primero define los serializadores
