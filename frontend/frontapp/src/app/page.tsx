3654a1e6e8cd91e03f1424952bfc147a08825bc9
Adrielram <adriel_ram@hotmail.com>
2025-02-02 21:56:48 -0300

    merge

--- frontend/frontapp/src/app/page.tsx
+++ frontend/frontapp/src/app/page.tsx
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
 const Page = () => {  
 
   return (
     <div>
       Hola
-=======
-"use client"
-
-import { useState, useEffect } from "react";
-
-
-const Page = () => {
-
-
-  return (
-    <div>
-      <h1>hiii</h1>
->>>>>>> PacienteInterface
     </div>
   );
 };
